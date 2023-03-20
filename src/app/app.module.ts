12ed18c5f337952ed5e3a11f6ffc15c843c730bf
Abayani36 <anenunez15@gmail.com>
2023-03-20 21:15:54 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
@@ -13,7 +12,7 @@ import {RouterModule ,Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   { path:'products', component: ProductListComponent },
-  { path:'products/:id', component: ProductPageComponent },
+  { path:'product/:id', component: ProductPageComponent },
   { path:'', redirectTo: 'products', pathMatch: 'full' },
 ];
 
